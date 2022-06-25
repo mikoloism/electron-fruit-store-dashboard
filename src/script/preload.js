630af9f193ef6aad5be4130a4b9aa2e101c9aec5
miko-github <mikoloism-github@gmail.com>
2022-06-25 13:20:08 +0430

    feat (router) : support SPA Routers without frameworks and create router functions to hot-reloading page

--- src/script/preload.js
+++ src/script/preload.js
@@ -1,5 +1,33 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
+
+const nativePath = require('path');
+const { isLoggedIn } = require('../model/database.js');
+
+const path = (HTMLFile) =>
+	nativePath.join(__dirname, 'pages', `${HTMLFile}.html`);
+
+const ROUTERS = {
+	'/': isLoggedIn() ? path('dashboard') : path('login'),
+	'/login': path('login'),
+};
+
+function route(ev) {
+	let event = ev || window.event;
+	event.preventDefault();
+	window.history.pushState({}, '', event.target.href);
+	handleLocationUpdate();
+}
+
+async function handleLocationUpdate() {
+	const currentPath = window.location.pathname;
+	const currentRoute = currentPath ? ROUTERS[currentPath] : ROUTERS[404];
+	const viewContent = await fetch(currentRoute)
+		.then((res) => res.text())
+		.catch((err) => new Error(err));
+	document.querySelector('#app').innerHTML = viewContent;
+}
+
 window.addEventListener('DOMContentLoaded', () => {
 	// code
-})
+});
