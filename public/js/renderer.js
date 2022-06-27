12f5208fb34d08b3514492798767372c01617398
miko-github <mikoloism-github@gmail.com>
2022-06-27 19:41:36 +0430

    chor (renderer) : use renderer as single client script file and supporting axois, jquery and also simple initial code for changes and test

--- public/js/renderer.js
+++ public/js/renderer.js
@@ -1,3 +1,24 @@
+import axios from 'axios';
+import $ from 'jquery';
+
+// api constant and method
+const api = axios.create({});
+
+// login page selectors
+const $login = {
+	button: $('#login-button'),
+	username: $('#login-username'),
+	password: $('#login-password'),
+};
+
+// dashboard page selectors
+const $dashboard = {};
+
 (function () {
-	// code
+	$login.button.on('click', () => {
+		api.post('/login', {
+			username: $login.username.val(),
+			password: $login.password.val(),
+		});
+	});
 })();
