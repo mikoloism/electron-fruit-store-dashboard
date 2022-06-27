c49d0f056796e91c110c95d3bba56d90e2938f8a
miko-github <mikoloism-github@gmail.com>
2022-06-27 12:07:17 +0430

    fix (api-express) : using seprate end-point instead seprate server port

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const path = require('path');
-const router = require('./src/router.js');
-
-const api = express();
-api.use('/', (req, res) => {
-	res.send('[API] : successfully!');
-});
+const viewRouter = require('./src/router/view.js');
+const apiRouter = require('./src/router/api.js');
 
 const app = express();
 
@@ -14,29 +10,17 @@ app.use(
 	'/static/css',
 	express.static(__dirname + '/node_modules/normalize.css'),
 );
-app.get('/', (req, res) => {
-	return res.sendFile(path.resolve('src', 'pages', 'login.html'));
-	// return res.sendFile(path.resolve('public', 'index.html'));
-});
 
-const APP_SERVER_PORT = 3000;
-const API_SERVER_PORT = 8080;
-module.exports.api = () =>
-	new Promise((resolve, reject) => {
-		api.listen(API_SERVER_PORT, () => {
-			console.log(
-				`[API_SERVER] : listening on http://127.0.0.1:${API_SERVER_PORT}`,
-			);
-			return resolve(API_SERVER_PORT, api);
-		});
-	});
+app.use('/view', viewRouter);
+app.use('/api', apiRouter);
 
+const SERVER_PORT = 3000;
 module.exports.app = () =>
 	new Promise((resolve, reject) => {
-		return app.listen(APP_SERVER_PORT, () => {
+		return app.listen(SERVER_PORT, () => {
 			console.log(
-				`[MAIN_SERVER] : listening on http://127.0.0.1:${APP_SERVER_PORT}`,
+				`[MAIN_SERVER] : listening on http://127.0.0.1:${SERVER_PORT}`,
 			);
-			return resolve(APP_SERVER_PORT, app);
+			return resolve(SERVER_PORT, app);
 		});
 	});
