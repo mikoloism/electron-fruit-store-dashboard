cf7491508e76fef6949fcb170321e0313e57e566
miko-github <mikoloism-github@gmail.com>
2022-06-27 18:51:02 +0430

    feat (view-router) : adding basic end-points

--- src/router/view.js
+++ src/router/view.js
@@ -1,3 +1,26 @@
 const router = require('express').Router();
+const path = require('path');
+// TODO const { isLoggedIn } = require('../controller');
+const isLoggedIn = () => false;
+
+// helper functions
+const page = (HTMLFile) =>
+	path.join(__dirname, '..', 'pages', `${HTMLFile}.html`);
+
+// declare constant file path
+const VIEW_LOGIN = page('login');
+const VIEW_DASHBOARD = page('dashboard');
+
+// view-router middleware
+router.use((req, res, next) => {
+	res.view = (HTMLPath) => res.sendFile(HTMLPath);
+	return next();
+});
+
+router.get('/', (req, res) =>
+	res.view(!isLoggedIn() ? VIEW_LOGIN : VIEW_DASHBOARD),
+);
+router.get('/login', (req, res) => res.view(VIEW_LOGIN));
+router.get('/dashboard', (req, res) => res.view(VIEW_DASHBOARD));
 
 module.exports = router;
