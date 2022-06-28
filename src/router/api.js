541a2f8d13612e04d44ae15b6af7e5e8bfd33b2e
miko-github <mikoloism-github@gmail.com>
2022-06-28 22:06:09 +0430

    feat (api-router) : rest api for user/auth endpoints created

--- src/router/api.js
+++ src/router/api.js
@@ -1,3 +1,10 @@
+/* NOTE : endpoint of router start by "/api/*" */
 const router = require('express').Router();
+const Controller = require('../controller/index.js');
+
+router.get('/login', Controller.User.getIsLoggedIn);
+router.post('/login', Controller.User.login);
+router.delete('/login', Controller.User.logout);
+router.post('/logout', Controller.User.logout);
 
 module.exports = router;
