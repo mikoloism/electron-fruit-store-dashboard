f22593703ca6a5e8f5f952c00e99c8cf37726cde
miko-github <mikoloism-github@gmail.com>
2022-06-28 22:04:41 +0430

    fix (user-controller) : declare getIsloggedIn method

--- src/controller/User.js
+++ src/controller/User.js
@@ -7,6 +7,21 @@ class User {
 		User.isLoggedIn = val;
 		return User.isLoggedIn;
 	}
+	static getIsLoggedIn(req, res) {
+		if (User.isLoggedIn) {
+			return res.send({
+				isLoggedIn: true,
+				message: 'You are logged in',
+				target: [],
+			});
+		}
+
+		return res.send({
+			isLoggedIn: false,
+			message: 'You are not logged in',
+			target: [],
+		});
+	}
 
 	static login(req, res) {
 		const { username, password } = req.body || {
