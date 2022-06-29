a5b76ab3b56fd3d4fde14da261ea7a89a0268ac9
miko-github <mikoloism-github@gmail.com>
2022-06-29 19:38:03 +0430

    fix (renderer) : convert api response to json format and using it in promise

--- public/js/renderer.js
+++ public/js/renderer.js
@@ -30,7 +30,8 @@ jQuery(document).ready(function ($) {
 				password: $login.password.val(),
 			},
 		})
-			.then((res) => console.log(res))
+			.then((res) => res.json())
+			.then((data) => console.log(data))
 			.catch((err) => console.log(err));
 	});
 });
