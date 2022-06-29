e4917bc8065c3bba94d3bf46935d57c1d47a6c94
miko-github <mikoloism-github@gmail.com>
2022-06-29 10:27:28 +0430

    chor (view-router) : 404 page supported and login-check before requeste added middleware

--- src/router/view.js
+++ src/router/view.js
@@ -1,15 +1,28 @@
 const router = require('express').Router();
 const path = require('path');
-// TODO const { isLoggedIn } = require('../controller');
-const isLoggedIn = () => false;
+const UserController = require('../controller/User.js');
 
 // helper functions
+const isLoggedIn = () => UserController.isLoggedIn;
+const isLoginView = (path) => {
+	const regex = new RegExp('//login', 'gi');
+	return regex.test(path);
+};
+const checkView = (req, res, next) => {
+	if (!UserController.isLoggedIn) {
+		res.redirect('/view/login');
+		return;
+	} else {
+		return next();
+	}
+};
 const page = (HTMLFile) =>
 	path.join(__dirname, '..', 'pages', `${HTMLFile}.html`);
 
 // declare constant file path
 const VIEW_LOGIN = page('login');
 const VIEW_DASHBOARD = page('dashboard');
+const VIEW_404 = page('404');
 
 // view-router middleware
 router.use((req, res, next) => {
@@ -17,10 +30,10 @@ router.use((req, res, next) => {
 	return next();
 });
 
-router.get('/', (req, res) =>
-	res.view(!isLoggedIn() ? VIEW_LOGIN : VIEW_DASHBOARD),
-);
+// view-router endpoint
+router.get('/', checkView);
 router.get('/login', (req, res) => res.view(VIEW_LOGIN));
-router.get('/dashboard', (req, res) => res.view(VIEW_DASHBOARD));
+router.get('/dashboard', checkView, (req, res) => res.view(VIEW_DASHBOARD));
+router.all('*', (req, res) => res.status(404).view(VIEW_404));
 
 module.exports = router;
