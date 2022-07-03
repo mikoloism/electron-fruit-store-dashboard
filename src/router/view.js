01c78711bfc36d525ae1af35ae3e1546135a1197
miko-github <mikoloism-github@gmail.com>
2022-07-03 13:58:13 +0430

    feat (pages, view-router) : create pages and view router

--- src/router/view.js
+++ src/router/view.js
@@ -18,6 +18,11 @@ function page(HTMLFile) {
 // declare constant file path
 const VIEW_LOGIN = page('login');
 const VIEW_DASHBOARD = page('dashboard');
+const VIEW_PRODUCT = page('product');
+const VIEW_INVOICE = page('invoice');
+const VIEW_SALER = page('saler');
+const VIEW_CUSTOMER = page('customer');
+const VIEW_PROFILE = page('profile');
 const VIEW_404 = page('404');
 
 // view-router middleware
@@ -30,6 +35,11 @@ router.use((req, res, next) => {
 router.get('/', checkLoggedIn);
 router.get('/login', (req, res) => res.view(VIEW_LOGIN));
 router.get('/dashboard', checkLoggedIn, (req, res) => res.view(VIEW_DASHBOARD));
+router.get('/product', (req, res) => res.view(VIEW_PRODUCT));
+router.get('/invoice', (req, res) => res.view(VIEW_INVOICE));
+router.get('/saler', (req, res) => res.view(VIEW_SALER));
+router.get('/customer', (req, res) => res.view(VIEW_CUSTOMER));
+router.get('/profile', (req, res) => res.view(VIEW_PROFILE));
 router.all('*', (req, res) => res.status(404).view(VIEW_404));
 
 module.exports = router;
