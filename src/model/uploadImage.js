1b4298481232986649925d46d44d303e6f0d89a9
miko-github <mikoloism-github@gmail.com>
2022-07-09 12:38:00 +0430

    feat (uploadImage) : support images savign by sharp and multer and export as object module

--- src/model/uploadImage.js
+++ src/model/uploadImage.js
@@ -1,14 +1,34 @@
 const multer = require('multer');
+const sharp = require('sharp');
+const path = require('path');
+const uuid = require('uuid');
+
+const UPLOAD_PATH = path.join(__dirname, 'database', 'uploads');
+
+function generateUniqueName() {
+	return `${uuid.v4()}.jpeg`;
+}
+
+// NOTE : req.file.buffer from uploadImage()
+// NOTE : fileName generated from generateUniqueName()
+// NOTE : call in uploadImage()
+function saveImage(fileBuffer, fileName) {
+	return sharp(fileBuffer)
+		.jpeg({ quality: 40 })
+		.toFile(`${UPLOAD_PATH}/${fileName}`);
+}
 
 const Storage = multer.diskStorage({
 	destination: function (req, file, callback) {
-		callback(null, './uploads');
+		callback(null, './../../database/uploads');
 	},
 	filename: function (req, file, callback) {
-		callback(null, file.fieldname + '-' + Date.now());
+		callback(null, generateUniqueName());
 	},
 });
 
-const uploadImage = multer({ storage: Storage }).single('userPhoto');
+const uploadImage = multer({ storage: Storage }).single('uploaded-images');
 
-module.exports = uploadImage;
+module.exports.uploadImage = uploadImage;
+module.exports.generateUniqueName = generateUniqueName;
+module.exports.saveImage = saveImage;
