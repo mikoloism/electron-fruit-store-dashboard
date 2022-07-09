3674d78a5dd0193426cced90f1990c3205329b4d
miko-github <mikoloism-github@gmail.com>
2022-07-09 17:41:08 +0430

    fix (fruit-model) : adding semi-colon at the end of query string

--- src/model/Fruit.js
+++ src/model/Fruit.js
@@ -9,21 +9,21 @@ class Fruit {
 		cost INTEGER DEFAULT 0,
 		quantity REAL DEFAULT 0,
 		image TEXT
-	)`,
-		INSERT: `INSERT INTO Fruit (name, cost, quantity, image) VALUES (?, ?, ?, ?)`,
+	);`,
+		INSERT: `INSERT INTO Fruit (name, cost, quantity, image) VALUES (?, ?, ?, ?);`,
 		UPDATE: {
-			ALL: `UPDATE Fruit SET name = ?, cost = ?, quantity = ?, image = ? WHERE id = ?`,
-			NAME: `UPDATE Fruit SET name = ? WHERE id = ?`,
-			COST: `UPDATE Fruit SET cost = ? WHERE id = ?`,
-			QUANTITY: `UPDATE Fruit SET quantity = ? WHERE id = ?`,
-			IMAGE: `UPDATE Fruit SET image = ? WHERE id = ?`,
+			ALL: `UPDATE Fruit SET name = ?, cost = ?, quantity = ?, image = ? WHERE id = ?;`,
+			NAME: `UPDATE Fruit SET name = ? WHERE id = ?;`,
+			COST: `UPDATE Fruit SET cost = ? WHERE id = ?;`,
+			QUANTITY: `UPDATE Fruit SET quantity = ? WHERE id = ?;`,
+			IMAGE: `UPDATE Fruit SET image = ? WHERE id = ?;`,
 		},
 		SELECT: {
-			ALL: `SELECT * FROM Fruit`,
-			BY_ID: `SELECT * FROM Fruit WHERE id = ?`,
+			ALL: `SELECT * FROM Fruit;`,
+			BY_ID: `SELECT * FROM Fruit WHERE id = ?;`,
 		},
-		DELETE: `DELETE FROM Fruit WHERE id = ?`,
-		DROP: `DROP TABLE IF EXISTS Fruit`,
+		DELETE: `DELETE FROM Fruit WHERE id = ?;`,
+		DROP: `DROP TABLE IF EXISTS Fruit;`,
 	};
 
 	static init() {
