36ccf943bcfaa6611f1d9de749892d6f5e1a27b2
miko-github <mikoloism-github@gmail.com>
2022-07-10 12:29:57 +0430

    fix (api-router) : use middleware receiveImage + fix (api-router) : seprate uploadImagje and insert data controller in the post method

--- src/router/api.js
+++ src/router/api.js
@@ -1,6 +1,7 @@
 /* NOTE : endpoint of router start by "/api/*" */
 const router = require('express').Router();
 const Controller = require('../controller/index.js');
+const { receiveImage } = require('../model/uploadImage.js');
 
 router.get('/login', Controller.User.getIsLoggedIn);
 router.post('/login', Controller.User.login);
@@ -8,6 +9,7 @@ router.delete('/login', Controller.User.logout);
 router.post('/logout', Controller.User.logout);
 
 router.get('/fruit', Controller.Fruit.read);
-// TODO : router.get('/fruit', Controller.User.checkLogin, Controller.Fruit.read);
+router.post('/fruit/image', receiveImage, Controller.Fruit.uploadImage);
+router.post('/fruit/data', Controller.Fruit.create);
 
 module.exports = router;
